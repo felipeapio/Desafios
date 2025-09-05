506e471defb4f35487083529dac9b0bcc7760fa0
felipe apio de souza <felipeapiodesouza@hotmail.com>
2025-09-05 14:17:52 -0300

    Iniciando a atividade 3.1 a respeito de um metodo para Fibonacci

---
+++ Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1 - Fibonacci Resultado esperado: 8 que seria o m5, a entrada consiste em 6 porque inicamos do m0
+        System.out.println("Resultado Fibonacci: " + fibonacci(6));
+    }
+
+    /*
+     1 - Fibonacci
+     
+     -- Criar uma função em sua linguagem preferida. A função deve receber um
+     numero N >= 0 (deve validar o input para a função), e retornar o valor
+     correspondente desse número na sequência Fibonacci. EX. fib(0) =0; fib(1) =
+     1; fib(2) = 1; fib(3) = 2; fib(5) = 5; fib(6) = 8.
+     
+     --- Criar uma função recursiva que resolva Fibonacci
+     
+     --- Criar uma função linear que resolva Fibonacci
+     
+     */
+    public static int fibonacci(int n) {
+        /*
+         pelo que entendi a sequencia a sequencia de fibonacci consiste no seguinte
+         se inicio com um casal de coelhos jovens, e conforme no primeiro mes eles
+         ainda sao um casal
+         porém sao um casal de adultos
+         utilizei esse video pra entender a sequencia:
+         https://www.youtube.com/watch?v=cHZWZhHQq4g
+         */
+        if (n == 0 || n == 1) {
+            return 1;
+            // temos apenas um casal de coelhos, infantil no primeiro mes e adulto no 2, ele
+            // so ira reproduzir a partir do 2 mes
+        } else {
+            /*
+            mas aqui ja complica um pouco essa charada,
+            a partir do segundo mes nos teremos 2 casais de coelhos, um adulto e um jovem
+            a partir do terceiro mes nos ja teremos os casais de adultos, e o casal que
+            nasceu no segundo mes, entao nosso resultado sera 3
+            porque 3? o casal de coelhos reproduz a cada 2 meses, porque no primeiro 1
+            nascido sao criancas e no 2 mes sao adultos (mas acabaram de se conhecer
+            vamos por assim)
+            logo eles se reproduzem de 3 em 3 meses, por isso no 4 mes o resultado sera:
+            5
+            m0: 1casalJovem 1 = 1
+            m1: 1cAdultos 1 = 1
+            m2: 1cAdultos 1casalJovem 2 =2
+            m3: 1cAdultos 1cAdultos 1casalJovem = 3
+            m4: 1cAdultos 1cAdultos 1cAdultos 1casalJovem 1casalJovem = 5
+            m5: 1cAdultos 1cAdultos 1cAdultos 1cAdultos 1cAdultos 1casalJovem 1casalJovem 1casalJovem = 8
+            ok com base nisso
+             */
+          
+            int temp[] = new int[n];
+            
+            for (int i = 0; i < temp.length; i++) {
+                if (i == 0 || i == 1) {
+                    temp[i] = fibonacci(i);
+                }else{
+                    temp[i] = temp[i - 1] + temp[i - 2]; // a sequencia constiste na soma dos dois numeros anteriores
+                }
+                
+                System.out.println("Coelho:" +  i + "=" + temp[i]);
+            }
+
+            return temp[temp.length - 1];
+        }
+    }
+}
